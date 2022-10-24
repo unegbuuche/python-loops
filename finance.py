6fa1f118c6800fe02b6e30588f6be9b85b53f77a
kingsley-people-box <kingsley@peoplebox.ai>
2022-10-24 07:39:10 +0100

    assignment codes

---
+++ finance.py
@@ -0,0 +1,19 @@
+
+# Ahmed, EIT Fellow Financial Statement
+capital = 50000
+print('************************************')
+print(f'    Starting Capital --  #{capital:,} GHC     ')
+print('************************************')
+print('')
+print('')
+print(f'Marketing Cost                               {(25/100*capital):,}  GHC')
+print(f'Operational Expenses                         {(50/100*capital):,}  GHC')
+print(f'Customer Acquisition                         {(25/100*capital):,}  GHC')
+print('')
+print('-----------------------------------------------------')
+print(f'Total                                        {(capital * 1.0):,} GHC')
+print('')
+print('')
+print('')
+print('')
+print(f"Since it cost him 5GHC per customer then he acquired                    {int((25/100*capital) / 5)} customers")
---
+++ loops.py
@@ -0,0 +1,19 @@
+# for loop for even numbers between 1 - 30
+for i in range(2,31, 2) :
+    print(i)
+
+# while loop for even numbers between 1 - 30
+num = 2
+while num <= 30 :
+    print(num)
+    num += 2
+
+# for loop for odd numbers between 1 - 30
+for i in range(1,30, 2) :
+    print(i)
+
+# while loop for odd numbers between 1 - 30
+num = 1
+while num <= 30 :
+    print(num)
+    num += 2
